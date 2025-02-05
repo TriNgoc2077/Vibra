fd1d311dc6881422a2fb3398f90eb6f8d497ab07
Tri Ngoc <caotringoc11@gmail.com>
2025-02-05 14:00:13 +0700

    audio preview

--- Public/Admin/Js/script.js
+++ Public/Admin/Js/script.js
@@ -1,3 +1,18 @@
+// show alert
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+    const time = parseInt(showAlert.getAttribute("data-time"));
+    const closeAlert = showAlert.querySelector("[close-alert]")
+    
+    setTimeout(() => {
+        showAlert.classList.add("alert-hidden");
+    }, time);
+    closeAlert.addEventListener("click", () => {
+        showAlert.classList.add("alert-hidden");
+    });
+}
+// end show alert
+
 // Preview image upload
 const uploadImage = document.querySelector("[upload-image]");
 if (uploadImage){
@@ -13,17 +28,21 @@ if (uploadImage){
     });
 }
 // End preview image upload
-// show alert
-const showAlert = document.querySelector("[show-alert]");
-if (showAlert) {
-    const time = parseInt(showAlert.getAttribute("data-time"));
-    const closeAlert = showAlert.querySelector("[close-alert]")
-    
-    setTimeout(() => {
-        showAlert.classList.add("alert-hidden");
-    }, time);
-    closeAlert.addEventListener("click", () => {
-        showAlert.classList.add("alert-hidden");
+// play audio upload
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio){
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+    const source = uploadAudio.querySelector('source');
+
+    uploadAudioInput.addEventListener("change", e => {
+        if (e.target.files.length) {
+            uploadAudioPlay.classList.remove('d-none');
+            uploadAudioPlay.classList.add('d-flex');
+            const audio = URL.createObjectURL(e.target.files[0]);
+            source.src = audio;
+            uploadAudioPlay.load();
+        }
     });
 }
-// end show alert
+// End pplay audio upload
