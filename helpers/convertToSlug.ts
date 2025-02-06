8365420106ede68f4687ba2a9fdff17af35dbe0e
TriNgoc2077 <caotringoc11@gmail.com>
2025-02-06 22:15:50 +0700

    Delete helpers directory

--- helpers/convertToSlug.ts
+++
@@ -1,6 +0,0 @@
-import slugify from 'slugify';
-import removeAccents from 'remove-accents';
-export const convertToSlug = (text: string): string => {
-    const slug = slugify(removeAccents(text), { lower: true, strict: true });
-    return slug;
-}
--- helpers/removeTimestamps.ts
+++
@@ -1,3 +0,0 @@
-export const removeTimestamps = (lyrics: string): string => {
-    return lyrics.replace(/\[\d{2}:\d{2}\.\d{2}\]\s*/g, '');
-  };
