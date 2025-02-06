280bfa178946d62448adceab58134de147b7cda7
TriNgoc2077 <caotringoc11@gmail.com>
2025-02-06 20:27:16 +0700

    Delete dist directory

--- dist/Public/Admin/Js/tinymce-config.js
+++
@@ -1,7 +0,0 @@
-tinymce.init({
-    selector: 'textarea.textarea-mce',
-    plugins: 'advlist link image list',
-    entity_encoding: 'raw',
-    images_upload_url: '/admin/upload',
-    automatic_uploads: true,
-  });
--- dist/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.systemConfig = void 0;
-const PREFIX_ADMIM = '/admin';
-exports.systemConfig = {
-    prefixAdmin: PREFIX_ADMIM
-};
