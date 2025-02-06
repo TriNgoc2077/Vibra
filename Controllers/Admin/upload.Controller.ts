063d8f21cef0703fb8bee095e6e7450326d1888f
TriNgoc2077 <caotringoc11@gmail.com>
2025-02-06 20:26:18 +0700

    Delete Controllers directory

--- Controllers/Admin/upload.Controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express"
-export const index = (req: Request, res: Response) => {
-    console.log(req.body);
-    res.json({
-        location: req.body.file
-    });
-}
