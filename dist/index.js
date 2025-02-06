c739c7d68c363183fb4880cf6a15eae7dca9f25b
Tri Ngoc <caotringoc11@gmail.com>
2025-02-06 21:24:54 +0700

    fix static file: public

--- dist/index.js
+++ dist/index.js
@@ -59,7 +59,7 @@ app.use((0, method_override_1.default)('_method'));
 app.use('/tinymce', express_1.default.static(path_1.default.join(__dirname, 'node_modules', 'tinymce')));
 app.use((0, cors_1.default)({ credentials: true }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(`${__dirname}/Public`));
+app.use(express_1.default.static(`${__dirname}/public`));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, express_session_1.default)({
--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ app.use(cors({credentials: true}));
 
 app.use(cookieParser());
 
-app.use(express.static(`${__dirname}/Public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
